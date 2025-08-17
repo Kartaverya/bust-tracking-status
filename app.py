e0b143728df782b9a0f6cd7104ac181fc18520d8
Harsha <Harshagundi123@gmail.com>
2025-08-17 18:47:49 +0530

    port binding for render

--- app.py
+++ app.py
@@ -25,4 +25,7 @@ def index():
     return render_template("index.html", bus_status=bus_status)
 
 if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0",port=port)
     app.run(debug=True)
